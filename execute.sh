e2eed08652df5977a2a7bdeae73cd90f1949a4d7
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2019-04-19 17:11:48 +0200

    Update execute.sh

--- execute.sh
+++ execute.sh
@@ -1,11 +1,17 @@
+FILE='data/berlin-latest.osm.pbf'
+
+# Maven build MinimalGraphHopper
 if [ "$1" == 'BUILD' ]; then
 	mvn clean compile assembly:single -U -e
 fi
 
-FILE='data/berlin-latest.osm.pbf'
+# Make data/ folder if not exists
+mkdir -p data/
 
+# Download data from Geofabrik
 if [ ! -f "$FILE" ]; then
 	wget http://download.geofabrik.de/europe/germany/berlin-latest.osm.pbf -O $FILE
 fi
 
+# Run server with configuration from config.yml
 java -jar target/minimal-0.0.1-SNAPSHOT-jar-with-dependencies.jar server config.yml
